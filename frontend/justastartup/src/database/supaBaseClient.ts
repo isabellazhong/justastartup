ba35fe395fd51946ea5650073615edb62e1afd1f
Isabella Zhong <isabellazhong888@gmail.com>
2025-10-05 08:50:48 -0400

    fix minor mistakes

--- frontend/justastartup/src/database/supaBaseClient.ts
+++ frontend/justastartup/src/database/supaBaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_PROJECT_URL
-const supabaseKey = import.meta.env.VITE_API_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey);
