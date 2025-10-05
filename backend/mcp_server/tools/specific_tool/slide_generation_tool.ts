c3b58022cb0b7fb2fd232cda831bd211f9d01a16
Isabella Zhong <isabellazhong888@gmail.com>
2025-10-04 20:00:09 -0400

    REDO/DELETE EVERYTHING

--- backend/mcp_server/tools/specific_tool/slide_generation_tool.ts
+++
@@ -1,6 +0,0 @@
-export interface SlideDeck {
-  buffer: Buffer;
-  filename?: string;
-  format?: "application/pdf" | "application/vnd.openxmlformats-officedocument.presentationml.presentation";
-}
-
